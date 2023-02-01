94ff93b5a2df359e5e0f304a59de9c877d64d39c
JoaoPedro019 <joao17042003@gmail.com>
2023-02-01 02:15:30 -0300

    commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var numeroSecreto = parseInt(Math.random()*100)+1;
+var tentativa = 10;
+
+const chute = document.querySelector('#chute');
+
+function Game() {
+
+    while(chute != numeroSecreto) {
+        var chute = prompt("Olá, seja bem vindo ao jogo. Você tem 10 tentativas. Digite seu número de 0 a 100:")
+        if (chute == numeroSecreto) {
+          alert("Parabéns, você acertou!")
+      }
+        if (tentativa != numeroSecreto) {
+          tentativa = tentativa - 1
+      }
+        if (tentativa <= 0) {
+        alert("Você infelizmente perdeu, a resposta era " + númeroOculto + ", boa sorte na próxima")
+        break 
+      }
+        else if (chute > numeroSecreto) {
+          alert("Errou :( o número é menor que " + chute + ", tente novamente")
+      }
+        else if (chute < numeroSecreto) {
+          alert("Errou :( o número é maior que " + chute + ", tente novamente")
+      }
+      }
+}
